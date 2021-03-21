3e39879e194f4dcbf6dedc8b12a0dc4a28806a89
Sufi Mohamed <sufimohamed@outlook.com>
2021-03-21 16:25:34 +0000

    [CI] add deploy script and execute when on travis success

---
+++ ci/deploy.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+export AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}"
+export AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}"
+export BUCKET="${BUCKET}"
+export CLOUDFRONT_DISTRIBUTION_ID="${CLOUDFRONT_DISTRIBUTION_ID}"
+
+[[ -z "${AWS_ACCESS_KEY_ID}" ]] && echo "AWS_ACCESS_KEY_ID is missing"
+
+
+aws s3 sync build/ "s3://${BUCKET}"
+aws cloudfront create-invalidation -- distribution-id "${CLOUDFRONT_DISTRIBUTION_ID}" --path "/*"
